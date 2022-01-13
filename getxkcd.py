27550723f9109e9f5d262b9a58fd9e30f2f6f32d
Eeshaan <eeshaanbhat@gmail.com>
2022-01-13 14:55:09 +0530

    Added random comic functionality

--- getxkcd.py
+++ getxkcd.py
@@ -20,8 +20,32 @@ for i in urls:
 req2 = requests.get(imgurl)
 req2.raise_for_status()
 
-#Write to image file
-img = open(title, 'wb')
-for chunk in req2.iter_content(100000):
-    img.write(chunk)
+
+#Code to fetch random comic, works the same as above
+randomimgurl = ''
+randomtitle = ''
+
+randomreq = requests.get('https://c.xkcd.com/random/comic')
+randomreq.raise_for_status()
+
+randomsoup = bs4.BeautifulSoup(randomreq.text, 'html.parser')
+randomurls = randomsoup.select('#comic img')
+
+for i in randomurls:
+    randomimgurl += 'https:' + i['src']
+    for j in i['alt'].split(' '):
+        randomtitle += j
+
+randomimgreq = requests.get(randomimgurl)
+randomimgreq.raise_for_status()
+
+choice = input('Latest/Random (L/R):')
+if choice == 'L':
+    img = open(title, 'wb')
+    for chunk in req2.iter_content(100000):
+        img.write(chunk)
+elif choice == 'R':
+    img = open(randomtitle, 'wb')
+    for chunk in randomimgreq.iter_content(100000):
+        img.write(chunk)
 img.close()
