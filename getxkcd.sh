5731f81495ce33fac27842a2ca2aacb9a180c263
Prithvi Vishak <prithvivishak@gmail.com>
2022-01-18 11:23:42 +0530

    Update getxkcd.sh to ask for random comic

--- getxkcd.sh
+++ getxkcd.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-comicUrl=$(curl https://xkcd.com 2> /dev/null | grep -i "hotlinking" | grep -Eo 'https:[/|a-z|A-Z|\.|_]+?png' | head -n 1)
+read -n 1 -ep "Get random comic? [y|N]: " rand
+if [ $rand = "y" ]; then
+	comicUrl=$(curl -L https://c.xkcd.com/random/comic 2> /dev/null | grep -i "hotlinking" | grep -Eo 'https:[/|a-z|A-Z|\.|_]+?png' | head -n 1)
+else
+	comicUrl=$(curl -L https://xkcd.com 2> /dev/null | grep -i "hotlinking" | grep -Eo 'https:[/|a-z|A-Z|\.|_]+?png' | head -n 1)
+fi
+
 comicFile=$(basename "$comicUrl")
 wget "$comicUrl" &> /dev/null
 xdg-open "$comicFile"
